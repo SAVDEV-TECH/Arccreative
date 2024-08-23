e0ac6eb9f452a3b820d4693e2e3aa0895720cde7
Savdev <savde388@gmail.com>
2024-08-23 07:32:15 -0700

    u

--- src/component/BacktoTop.js
+++ src/component/BacktoTop.js
@@ -2,21 +2,27 @@ import React from 'react'
 import { FaArrowAltCircleDown } from 'react-icons/fa'
 
 function BacktoTop() {
-    const handleVisibleButton = () => {
-        const position = window.pageYOffset;
-        setSrollPosition(position);
+    // const handleVisibleButton = () => {
+    //     const position = window.pageYOffset;
+    //     setSrollPosition(position);
     
-        if (scrollPosition > 50) {
-          return setshowGoTop("goTop");
-        } else if (scrollPosition < 50) {
-          return setshowGoTop("goTopHidden");
-        }
-      };
-      useEffect(() => {
-        window.addEventListener("scroll", scrolling);
-      });
+    //     if (scrollPosition > 50) {
+    //       return setshowGoTop("goTop");
+    //     } else if (scrollPosition < 50) {
+    //       return setshowGoTop("goTopHidden");
+    //     }
+    //   };
+      // useEffect(() => {
+      //   window.addEventListener("scroll", scrolling);
+      // });
+      const scrollToTop=()=> {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
   return (
-    <div onClick={()=> scrolling} className='animate-bounce text-[20px] rounded-full flex items-center justify-center bg-purple-500 cursor-pointer fixed bottom-48 right-10 w-[50px] h-[50px]'>
+    <div onClick={scrollToTop}  className='animate-bounce text-[20px] text-white  rounded-full flex items-center justify-center bg-purple-500 z-40 cursor-pointer fixed bottom-48 right-10 w-[50px] h-[50px]'>
       <FaArrowAltCircleDown ></FaArrowAltCircleDown>
     </div>
   )
