69abfcd457215457cf367399a881fb2a11ee4431
Savdev <savde388@gmail.com>
2024-08-30 22:38:05 -0700

    u

--- src/component/Sign in & Login/Form.js
+++ src/component/Sign in & Login/Form.js
@@ -5,8 +5,8 @@ function Form() {
   return (
     <div className='w-[100%] flex  items-center justify-center md:justify-normal   h-screen '>
       <form className='w-[80%]   ml-0  md:w-[60%] py-5 px-3  items-center justify-center gap-7 flex flex-col md:ml-[100px] '>
-        <h1  className='text-2xl mb-3  w-[70%] font-bold text-left text-[0d0c22] font-sans'>Sign up to Arcreative</h1>
-        <Link className='w-[90%] sm:w-[70%] flex items-center justify-center gap-3  md:w-[426px] px-11 text-[14px] text-center text-white font-semibold py-[17px] rounded-full bg-black'>
+        <h1  className='text-[20px] md:text-2xl mb-3  w-[100%] md:w-[70%] font-bold text-left text-[0d0c22] font-sans'>Sign up to Arcreative</h1>
+        <Link className='w-[100%] sm:w-[70%] flex items-center justify-center gap-3  md:w-[426px] px-11 text-[14px] text-center text-white font-semibold py-[17px] rounded-full bg-black'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none" role="img" class="icon ">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M17.64 9.20419C17.64 8.56601 17.5827 7.95237 17.4764 7.36328H9V10.8446H13.8436C13.635 11.9696 13.0009 12.9228 12.0477 13.561V15.8192H14.9564C16.6582 14.2524 17.64 11.9451 17.64 9.20419Z" fill="#4285F4"></path>
 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.99976 18C11.4298 18 13.467 17.1941 14.9561 15.8195L12.0475 13.5613C11.2416 14.1013 10.2107 14.4204 8.99976 14.4204C6.65567 14.4204 4.67158 12.8372 3.96385 10.71H0.957031V13.0418C2.43794 15.9831 5.48158 18 8.99976 18Z" fill="#34A853"></path>
@@ -20,9 +20,9 @@ function Form() {
             <span className='text-[12px]'>or</span>
             <span className='w-[40%] h-[1px] block bg-gray-500'></span>
         </div>
-        < Link className='w-[90%] sm:w-[70%] md:w-[426px] px-11 text-[14px] mb-7 text-center font-semibold text-gray py-[17px] rounded-full bg-white border border-gray-400'> Continue  with  Email</Link>
+        < Link className='w-[100%] sm:w-[70%] md:w-[426px] px-11 text-[14px] mb-7 text-center font-semibold text-gray py-[17px] rounded-full bg-white border border-gray-400'> Continue  with  Email</Link>
 
-        <div className=' text-center text-[12px] w-[90%]   px-9'>
+        <div className=' text-center text-[12px] w-[100%]  md:w-[90%]  px-7 md:px-9'>
             <p className=' text-gray-500 font-medium'>By creating an account you agree with our <Link>Terms of Service, Privacy Policy,</Link>   and our default<Link> Notification Settings. </Link> </p>
         </div>
         <div>
--- src/component/Sign in & Login/LoginForm.js
+++ src/component/Sign in & Login/LoginForm.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 function LoginForm() {
   return (
     <div className='w-[100%] flex  items-center justify-center md:justify-normal   h-screen '>
-    <form className='w-[80%]   ml-0  md:w-[60%] py-5 px-3  items-center justify-center gap-7 flex flex-col md:ml-[100px] '>
-      <h1  className='text-2xl   w-[70%] font-bold text-left text-[0d0c22] font-sans'>Sign in to Arcreative</h1>
-      <Link className='w-[90%] sm:w-[70%] flex items-center justify-center gap-3  md:w-[426px] px-11 text-[14px] text-center border border-gray-400 text-black font-semibold py-[17px] rounded-full bg-white'>
+    <form className='w-[100%]  md:w-[45%]   ml-0   py-5 px-0  md:px-3 items-center justify-center gap-7 flex flex-col md:ml-[100px] '>
+      <h1  className='text-2xl    w-[100%] md:w-[70%] font-bold text-left text-[0d0c22] font-sans'>Sign in to Arcreative</h1>
+      <Link className='w-[100%]   sm:w-[70%] mx-auto flex items-center justify-center gap-3  md:w-[85%] px-11 text-[14px] text-center border border-gray-400 text-black font-semibold py-[17px] rounded-full bg-white'>
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none" role="img" class="icon ">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M17.64 9.20419C17.64 8.56601 17.5827 7.95237 17.4764 7.36328H9V10.8446H13.8436C13.635 11.9696 13.0009 12.9228 12.0477 13.561V15.8192H14.9564C16.6582 14.2524 17.64 11.9451 17.64 9.20419Z" fill="#4285F4"></path>
 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.99976 18C11.4298 18 13.467 17.1941 14.9561 15.8195L12.0475 13.5613C11.2416 14.1013 10.2107 14.4204 8.99976 14.4204C6.65567 14.4204 4.67158 12.8372 3.96385 10.71H0.957031V13.0418C2.43794 15.9831 5.48158 18 8.99976 18Z" fill="#34A853"></path>
@@ -15,27 +15,28 @@ function LoginForm() {
 </svg>
       <p>Sign in With  Google</p> 
        </Link>
-      <div className='flex w-[85%] mx-auto items-center gap-4 justify-center'>
+      <div className='flex w-[100%] md:w-[85%] mx-auto items-center gap-4 justify-center'>
           <span className='w-[25%] h-[1px] block bg-gray-500'></span>
           <span className='text-[14px] font-semibold text-gray-400'>or Sign in with Email</span>
           <span className='w-[25%] h-[1px] block bg-gray-500'></span>
       </div>
-      <div className='flex flex-col gap-5 '>
-       <span className=' block  '>
+      <div className='flex flex-col w-[90%] mx-auto gap-5 '>
+       <div className='   w-full '>
         <h2 className='font-semibold'>Username or Email</h2>
-        <input className='w-[350px] md:w-[425px] shadow bg-gray-300 hover:shadow-pink-500 outline-none rounded-md py-[15px] block border border-gray-400' type='text'></input>
-       </span>
+        <input className='w-[100%]   shadow bg-gray-300 hover:shadow-pink-500 outline-none rounded-md py-[15px] block border border-gray-400' type='text'></input>
+       </div>
        <span className=' block  '>
        
         <span className='flex justify-between mb-1'> 
              <h2 className='font-semibold'>Password</h2>
              <Link to='/ResetPassword' className='font-semibold underline'>Forgot?</Link>
         </span>
-        
-        <input className='w-[350px] md:w-[425px] focus focus-within:shadow-pink-500 shadow hover:shadow-pink-500  outline-none rounded-md py-[17px] block  bg-gray-300  border border-gray-400' type='text'></input>
+        <div className='w-full relative'> 
+        <input className='w-[100%]  focus focus-within:shadow-pink-500 shadow hover:shadow-pink-500  outline-none rounded-md py-[17px]   bg-gray-300  border border-gray-400' type='text'></input>
+        </div>
        </span>
       </div>
-      <Link className='w-[90%] sm:w-[70%] md:w-[426px] px-11 text-[14px]  text-center font-semibold text-gray py-[15px] rounded-full bg-blue-950 border text-white border-gray-400'> Sign in</Link>
+      <Link className='w-[90%]  sm:w-[70%] md:w-[85%] px-11 text-[14px]  text-center font-semibold text-gray py-[15px] rounded-full bg-blue-950 border text-white border-gray-400'> Sign in</Link>
 
       <div>
           <p className='text-[14px] text-gray-500'> <Link to='/SignIn' className='underline'> Dont have an accoun?  Sign up</Link></p>
